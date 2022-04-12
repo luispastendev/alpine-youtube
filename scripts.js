2e58c5fafa36a5be4eb8f6e1a2459f6aa62d69ff
Luis Pastén <luispasten.dev@gmail.com>
2022-04-12 15:57:38 -0500

    Video 2

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const app = {
+    title: 'Todo List',
+    tasks: [
+        {
+            id: 1,
+            title: 'hacer de comer',
+            active: false
+        },
+        {
+            id: 2,
+            title: 'salir a correr',
+            active: false
+        },
+        {
+            id: 3,
+            title: 'dormir',
+            active: false
+        }
+    ]
+};
